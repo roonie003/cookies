edb597946d4a39d69399a45d34f0dbabb1e2412a
Roonie <57942792+roonie003@users.noreply.github.com>
2020-05-25 16:42:29 +0200

    Add files via upload

---
+++ process.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $color = $_POST['color'];
+
+    setcookie('name', $name, time()+3600);
+    setcookie('age', $age, time()+3600);
+    setcookie('color', $color, time()+3600);
+}
---
+++ retrieve.php
@@ -0,0 +1,8 @@
+<?php
+
+
+        echo " Your name is " . $_COOKIE['name'] ;
+        echo "<br>";
+        echo " Your age is " . $_COOKIE['age'];
+        echo "<br>";
+        echo " Your favourite color is " . $_COOKIE['color'];
